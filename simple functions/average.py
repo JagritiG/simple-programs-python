42142c12477cbf4d03297312c78410117cd88f38
santanusarma <jagritigoswami84@gmail.com>
2019-02-14 20:45:38 -0800

    ..

--- simple functions/average.py
+++ simple functions/average.py
@@ -1,4 +1,4 @@
-# Following program alculates the average of numbers in a given list.
+# Following program calculates the average of numbers in a given list.
 
 def Average_list_element(given_list):
     return sum(given_list) / len(given_list)
